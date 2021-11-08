cbe84bea77f9522ce82b242c8cec0b0ac750f141
Ankita17Talwar <talwar17ankita@gmail.com>
2021-11-08 21:17:51 +0530

    TSNE

--- Hierarchical_Clustering_Stocks.py
+++ Hierarchical_Clustering_Stocks.py
@@ -23,5 +23,30 @@ mergings = linkage(normalized_data, method='complete')
 dendrogram(mergings, labels=companies, leaf_rotation=90, leaf_font_size=6)
 plt.show()
 
+## TSNE
+
+# Import TSNE
+from sklearn.manifold import TSNE
+
+# Create a TSNE instance: model
+model = TSNE(learning_rate=50)
+
+# Apply fit_transform to normalized_data: tsne_features
+tsne_features = model.fit_transform(normalized_data)
+
+# Select the 0th feature: xs
+xs = tsne_features[:,0]
+
+# Select the 1th feature: ys
+ys = tsne_features[:,1]
+
+# Scatter plot
+plt.scatter(xs,ys,alpha=0.5)
+
+
+# Annotate the points
+for x, y, company in zip(xs, ys, companies):
+    plt.annotate(company, (x, y), fontsize=5, alpha=0.75)
+plt.show()
 
 
